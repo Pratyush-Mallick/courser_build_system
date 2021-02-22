58a58e241062efc3442dbf79b30b68162e70bb3d
Pratyush-Mallick <pratyush.cetb@gmail.com>
2021-02-22 10:40:13 -0700

    Addind Baseline Code
    Signed-off-by: Pratyush-Mallick <pratyush.cetb@gmail.com>

--- stats.c
+++ stats.c
@@ -12,7 +12,7 @@
  * @file stats.c 
  * @brief Implementation of the statistics library
  *
- * This file contains algorithms to implement the following operations:
+ * This contains algorithms to implement the following operations:
  * Maximum, Minimum, Mean, Median, Sorting and Printing on a given data array
  *
  * @author Pratyush Mallick
@@ -20,3 +20,41 @@
  *
  */
 
+#include <stdio.h>
+#include "stats.h"
+
+void main() {
+
+}
+
+/* Statistics and Printing Functions Go Here */
+
+void print_statistics(unsigned char ptr[], unsigned int len)
+{
+
+}
+
+void print_array(unsigned char ptr[], unsigned int len)
+{
+
+}
+
+unsigned char find_median(unsigned char ptr[], unsigned int len){
+
+}
+
+unsigned char find_mean(unsigned char ptr[], unsigned int len){
+
+}
+
+unsigned char find_maximum(unsigned char ptr[], unsigned int len){
+
+}
+
+unsigned char find_minimum(unsigned char ptr[], unsigned int len){
+
+}
+
+void sort_array(unsigned char ptr[], unsigned int len){
+
+}
--- stats.h
+++ stats.h
@@ -23,4 +23,109 @@
 #ifndef __STATS_H__
 #define __STATS_H__
 
+
+/**
+ * @brief Prints statistics to the console 
+ *
+ * This function takes as an pointer to the unsigned character array and length
+ * of the array as input parameters, prints out the the maximum, minimum, mean, 
+ * and median of the input data array to the console. 
+ *
+ * @param prt Unsigned character pointer to the array to be analysed
+ * @param len Length of the data array
+ *
+ * @return None
+ */
+
+void print_statistics(unsigned char ptr[], unsigned int len);
+
+/**
+ * @brief Prints input array to the console 
+ *
+ * This function takes as an pointer to the unsigned character array and length
+ * of the array as input parameters, prints out the data array to the console. 
+ *
+ * @param prt Unsigned character pointer to the array to be printed
+ * @param len Length of the data array
+ *
+ * @return None
+ */
+
+void print_array(unsigned char ptr[], unsigned int len);
+
+/**
+ * @brief Performs median operation on a input data array 
+ *
+ * This function takes as an pointer to the unsigned character array and length
+ * of the array as input parameters, perform median operation and returns the 
+ * result.
+ *
+ * @param prt Unsigned character pointer to the array to be printed
+ * @param len Length of the data array
+ *
+ * @return Median value of the data array
+ */
+
+unsigned char find_median(unsigned char ptr[], unsigned int len);
+
+/**
+ * @brief Performs the mean operation on a input data array 
+ *
+ * This function takes as an pointer to the unsigned character array and length
+ * of the array as input parameters, perform mean operation and returns the 
+ * result. 
+ *
+ * @param prt Unsigned character pointer to the array to be printed
+ * @param len Length of the data array
+ *
+ * @return Mean value of the data array
+ */
+
+unsigned char find_mean(unsigned char ptr[], unsigned int len);
+
+/**
+ * @brief Finds out the maximum element in a input data array 
+ *
+ * This function takes as an pointer to the unsigned character array and length
+ * of the array as input parameters, finds the element having the maximum value 
+ * and returns it. 
+ *
+ * @param prt Unsigned character pointer to the array to be printed
+ * @param len Length of the data array
+ *
+ * @return Maximum value in the data array
+ */
+
+unsigned char find_maximum(unsigned char ptr[], unsigned int len);
+
+/**
+ * @brief Finds out the minimum element in a input data array 
+ *
+ * This function takes as an pointer to the unsigned character array and length
+ * of the array as input parameters, finds the element having the minimum value 
+ * and returns it. 
+ *
+ * @param prt Unsigned character pointer to the array to be printed
+ * @param len Length of the data array
+ *
+ * @return Minimum value in the data array
+ */
+
+unsigned char find_minimum(unsigned char ptr[], unsigned int len);
+
+/**
+ * @brief Sorts the input array in descending order. 
+ *
+ * This function takes as an pointer to the unsigned character array and length
+ * of the array as input parameters, performs bubble sort operation on the 
+ * elements of the array.
+ *
+ * @param prt Unsigned character pointer to the array to be printed
+ * @param len Length of the data arra
+ *
+ * @return None
+ */
+
+void sort_array(unsigned char ptr[], unsigned int len);
+
 #endif /* __STATS_H__ */
