33c5d8f11d4020adbe6eeb3fb5f407cbc3221016
Pratyush-Mallick <pratyush.cetb@gmail.com>
2021-02-22 10:34:18 -0700

    Inital Commit
    Signed-off-by: Pratyush-Mallick <pratyush.cetb@gmail.com>

---
+++ stats.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file stats.c 
+ * @brief Implementation of the statistics library
+ *
+ * This file contains algorithms to implement the following operations:
+ * Maximum, Minimum, Mean, Median, Sorting and Printing on a given data array
+ *
+ * @author Pratyush Mallick
+ * @date 22-02-2021
+ *
+ */
+
---
+++ stats.h
@@ -0,0 +1,26 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file stats.h 
+ * @brief Statistics library header
+ *
+ * This file contains all the function declarations for performing  maximum, 
+ * minimum, mean, median, sorting and printing operation on a input array of  
+ * unsigned char data items 
+ *
+ * @author Pratyush Mallick
+ * @date 22-02-2021
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+#endif /* __STATS_H__ */
