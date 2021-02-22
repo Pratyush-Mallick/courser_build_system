a4d76a2b3c923c0a46a8714fcae2ba37e43495f3
Pratyush-Mallick <pratyush.cetb@gmail.com>
2021-02-22 10:44:03 -0700

    Added Implementation Code
    Signed-off-by: Pratyush-Mallick <pratyush.cetb@gmail.com>

--- stats.c
+++ stats.c
@@ -23,38 +23,92 @@
 #include <stdio.h>
 #include "stats.h"
 
+/* Size of the Data Set */
+#define SIZE   (40)
+
 void main() {
 
+  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+                              114, 88,   45,  76, 123,  87,  25,  23,
+                              200, 122, 150, 90,   92,  87, 177, 244,
+                              201,   6,  12,  60,   8,   2,   5,  67,
+                                7,  87, 250, 230,  99,   3, 100,  90};
+
+  unsigned int arr_len = SIZE;
+  
+  print_array(test, arr_len);
+  print_statistics(test, arr_len);
+  
 }
 
 /* Statistics and Printing Functions Go Here */
 
 void print_statistics(unsigned char ptr[], unsigned int len)
 {
-
+printf("\r\nMax = %d ",find_maximum(ptr,len));
+printf("\r\nMinimum = %d ",find_minimum(ptr,len));
+printf("\r\nMean = %d ",find_mean(ptr,len));
+printf("\r\nMedian = %d ",find_median(ptr,len));
 }
 
 void print_array(unsigned char ptr[], unsigned int len)
 {
-
+printf("\r\nARRAY");
+for(int i=0;i < len;i++)
+{
+printf("\r\n%d",ptr[i]);
+}
 }
 
 unsigned char find_median(unsigned char ptr[], unsigned int len){
-
+sort_array(ptr,len);
+return (ptr[len/2]);
 }
 
 unsigned char find_mean(unsigned char ptr[], unsigned int len){
+   __uint16_t mean = 0;
+   
+   for(unsigned int i = 0; i < len; i++) { 
+         mean += ptr[i] ;
+   }
+return (mean/len);
 
 }
 
 unsigned char find_maximum(unsigned char ptr[], unsigned int len){
+   unsigned char max = ptr[0];
+   
+   for(unsigned int i = 1; i < len; i++) {
+      if( max < ptr[i] ) 
+         max = ptr[i];
+   }
 
+return max;
 }
 
 unsigned char find_minimum(unsigned char ptr[], unsigned int len){
+   unsigned char min = ptr[0];
+   
+   for(unsigned int i = 1; i < len; i++) {
+      if( ptr[i] < min ) 
+         min = ptr[i];
+   }
 
+return min;
 }
 
 void sort_array(unsigned char ptr[], unsigned int len){
+unsigned char i,j;
 
+for(i=0;i<len;i++){
+for(j=0;j<len-i-1;j++){
+if(ptr[j] < ptr[j+1]){
+unsigned char temp = ptr[j];
+ptr[j] = ptr[j+1];
+ptr[j+1] = temp; 
+
+} 
+} 
 }
+}
+
